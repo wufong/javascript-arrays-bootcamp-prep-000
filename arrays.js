389896da629dd915a25b09d99576814843a3ea96
Mike Walker <jerry.kowal@gmail.com>
2018-05-19 16:33:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  'kitkat',
+  'skittles'
+  
+  ]
+
+
+
 function addElementToBeginningOfArray(array, element) {
   var newarray = [element, array];
   return newarray;
