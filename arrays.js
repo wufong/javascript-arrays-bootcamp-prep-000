f021cdf161e155898280d74b928985a645dcb7e6
Mike Walker <jerry.kowal@gmail.com>
2018-05-19 16:51:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
